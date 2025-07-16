e1cc9fa69f2d0fdd05974ac629e4ee41028f2a19
sujal-malviya <sujal.malviya2022@gmail.com>
2025-07-16 21:32:53 +0530

    Python

---
+++ new/patterns.py
@@ -0,0 +1,18 @@
+#Square pattern -
+for i in range(5):
+    for j  in range(5):
+        print("x",end=" ")
+    print()
+print()
+#right Angle triangle -
+for i in range(5):
+    for j in range(i):
+        print("x",end="")
+    print()
+print()
+
+for i in range(4):
+    print(" ")
+    for j in range(4):
+        print("x",end=" ")
+    print()
